4bd0c441c14749422b8d08126eba20ec9b4d2fda
Maevera <nicolas.lelong@neuf.fr>
2018-09-27 14:37:55 +0200

    ajout

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function Redirection(){
+ document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ.";
+}
+
+
+setTimeout(function Poputropcool(){
+   if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')){
+     Redirection()
+  } else { alert ("ok, je ne vais pas te déranger plus longtemps")}; }, 10000);
